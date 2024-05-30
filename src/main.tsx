12ff47c81b1d61c656c073d8b5f8015845a42e67
HamzaAlsarakbi <hamoz.sq78@gmail.com>
2024-05-30 18:25:39 -0400

    setup next-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider } from 'react-redux';
 import { store } from '@/app/store';
-
+import { NextUIProvider } from '@nextui-org/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
