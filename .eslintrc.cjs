56aed029d542b252a1cd0de5b6579e0039f6d10d
Hamza <hamoz.sq78@gmail.com>
2024-05-31 03:11:08 -0400

    linter tweaks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-strict-type-checked',
-    'plugin:@typescript-eslint/recommended-stylistic-type-checked',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
